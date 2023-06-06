eec69d42a0132a649d4c5f0eb755ce68b543fb04
sawan <sappler16@gmail.com>
2023-06-06 18:27:37 +0530

    feat: make feed

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
 
 const Rightbar = () => {
   return (
-    <Box
-      bgcolor="lightcoral"
-      flex={2}
-      p={2}
-      sx={{ display: { xs: "none", sm: "block" } }}
-    >
-      Rightbar
+    <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <Box position="fixed">
+        <Typography variant="h6" fontWeight={100}>
+          Online friends
+        </Typography>
+      </Box>
     </Box>
   );
 };
